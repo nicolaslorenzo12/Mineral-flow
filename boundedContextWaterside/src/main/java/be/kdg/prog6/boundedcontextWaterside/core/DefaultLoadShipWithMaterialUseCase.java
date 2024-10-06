9d06b19721e989f8fb6ec205a3f024fc05883014
niselo <nicolas.lorenzo@student.kdg.be>
2024-10-06 11:49:44 +0200

    code improved

--- boundedContextWaterside/src/main/java/be/kdg/prog6/boundedcontextWaterside/core/DefaultLoadShipWithMaterialUseCase.java
+++ boundedContextWaterside/src/main/java/be/kdg/prog6/boundedcontextWaterside/core/DefaultLoadShipWithMaterialUseCase.java
@@ -1,9 +1,12 @@
 package be.kdg.prog6.boundedcontextWaterside.core;
 
 import be.kdg.prog6.boundedcontextWaterside.domain.PurchaseOrder;
+import be.kdg.prog6.boundedcontextWaterside.domain.ShipmentOrder;
+import be.kdg.prog6.boundedcontextWaterside.domain.ShipmentStatus;
 import be.kdg.prog6.boundedcontextWaterside.ports.in.LoadShipWithMaterialCommand;
 import be.kdg.prog6.boundedcontextWaterside.ports.in.LoadShipWithMaterialUseCase;
 import be.kdg.prog6.boundedcontextWaterside.ports.out.LoadOrCreateShipmentOrderPort;
+import be.kdg.prog6.boundedcontextWaterside.ports.out.UpdateShipmentOrderPort;
 import be.kdg.prog6.boundedcontextWaterside.ports.out.UpdateWarehousePort;
 import be.kdg.prog6.common.exception.CustomException;
 import org.springframework.http.HttpStatus;
@@ -14,10 +17,13 @@ public class DefaultLoadShipWithMaterialUseCase implements LoadShipWithMaterialU
 
     private final LoadOrCreateShipmentOrderPort loadOrCreateShipmentOrderPort;
     private final UpdateWarehousePort updateWarehousePort;
+    private final UpdateShipmentOrderPort updateShipmentOrderPort;
 
-    public DefaultLoadShipWithMaterialUseCase(LoadOrCreateShipmentOrderPort loadOrCreateShipmentOrderPort, UpdateWarehousePort updateWarehousePort) {
+    public DefaultLoadShipWithMaterialUseCase(LoadOrCreateShipmentOrderPort loadOrCreateShipmentOrderPort, UpdateWarehousePort updateWarehousePort,
+                                              UpdateShipmentOrderPort updateShipmentOrderPort) {
         this.loadOrCreateShipmentOrderPort = loadOrCreateShipmentOrderPort;
         this.updateWarehousePort = updateWarehousePort;
+        this.updateShipmentOrderPort = updateShipmentOrderPort;
     }
 
     @Override
@@ -27,6 +33,12 @@ public class DefaultLoadShipWithMaterialUseCase implements LoadShipWithMaterialU
         PurchaseOrder purchaseOrder = loadOrCreateShipmentOrderPort.loadPurchaseOrder(poNumber)
                 .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "Purchase order was not found"));
 
+        ShipmentOrder shipmentOrder = loadOrCreateShipmentOrderPort.loadOrCreateShipmentOrder(poNumber)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "Shipment order was not found"));
+
+        shipmentOrder.checkIfShipmentOrderHasAlreadyHadThisStatus(ShipmentStatus.LOADED);
         purchaseOrder.getOrderLineList().forEach(orderLine -> updateWarehousePort.updateWarehouse(orderLine, purchaseOrder.getSellerUuid(), purchaseOrder.getBuyerUuid()));
+
+        updateShipmentOrderPort.updateShipmentOrder(shipmentOrder);
     }
 }
